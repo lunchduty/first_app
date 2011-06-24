754365f2cc9e1a28d79b5be59ec717c900d6153e
Monica Evanchik <monica.evanchik@gmail.com>
2011-06-24 13:23:48 -0400

    Changes to be committed:
    	new file:   rails_projects/first_app/.DS_Store
    	new file:   rails_projects/first_app/.gitignore.rtf
    	new file:   rails_projects/first_app/Gemfile
    	new file:   rails_projects/first_app/Gemfile.lock
    	new file:   rails_projects/first_app/README
    	new file:   rails_projects/first_app/Rakefile
    	new file:   rails_projects/first_app/app/controllers/application_controller.rb
    	new file:   rails_projects/first_app/app/helpers/application_helper.rb
    	new file:   rails_projects/first_app/app/views/layouts/application.html.erb
    	new file:   rails_projects/first_app/config.ru
    	new file:   rails_projects/first_app/config/.DS_Store
    	new file:   rails_projects/first_app/config/application.rb
    	new file:   rails_projects/first_app/config/boot.rb
    	new file:   rails_projects/first_app/config/database.yml
    	new file:   rails_projects/first_app/config/environment.rb
    	new file:   rails_projects/first_app/config/environments/development.rb
    	new file:   rails_projects/first_app/config/environments/production.rb
    	new file:   rails_projects/first_app/config/environments/test.rb
    	new file:   rails_projects/first_app/config/initializers/backtrace_silencers.rb
    	new file:   rails_projects/first_app/config/initializers/inflections.rb
    	new file:   rails_projects/first_app/config/initializers/mime_types.rb
    	new file:   rails_projects/first_app/config/initializers/secret_token.rb
    	new file:   rails_projects/first_app/config/initializers/session_store.rb
    	new file:   rails_projects/first_app/config/locales/en.yml
    	new file:   rails_projects/first_app/config/routes.rb
    	new file:   rails_projects/first_app/db/development.sqlite3
    	new file:   rails_projects/first_app/db/seeds.rb
    	new file:   rails_projects/first_app/doc/README_FOR_APP
    	new file:   rails_projects/first_app/lib/tasks/.gitkeep
    	new file:   rails_projects/first_app/log/development.log
    	new file:   rails_projects/first_app/log/production.log
    	new file:   rails_projects/first_app/log/server.log
    	new file:   rails_projects/first_app/log/test.log
    	new file:   rails_projects/first_app/public/404.html
    	new file:   rails_projects/first_app/public/422.html
    	new file:   rails_projects/first_app/public/500.html
    	new file:   rails_projects/first_app/public/favicon.ico
    	new file:   rails_projects/first_app/public/images/rails.png
    	new file:   rails_projects/first_app/public/index.html
    	new file:   rails_projects/first_app/public/javascripts/application.js
    	new file:   rails_projects/first_app/public/javascripts/controls.js
    	new file:   rails_projects/first_app/public/javascripts/dragdrop.js
    	new file:   rails_projects/first_app/public/javascripts/effects.js
    	new file:   rails_projects/first_app/public/javascripts/prototype.js
    	new file:   rails_projects/first_app/public/javascripts/rails.js
    	new file:   rails_projects/first_app/public/robots.txt
    	new file:   rails_projects/first_app/public/stylesheets/.gitkeep
    	new file:   rails_projects/first_app/script/rails
    	new file:   rails_projects/first_app/test/performance/browsing_test.rb
    	new file:   rails_projects/first_app/test/test_helper.rb
    	new file:   rails_projects/first_app/vendor/.DS_Store
    	new file:   rails_projects/first_app/vendor/plugins/.gitkeep

---
+++ rails_projects/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '06a0e78bbc3f339b0becdcd7e22251c6c428f8e15e6f6465f7600ae84b7c5a884ca668aec350b9983e4fb66af0e0a54aa5accb5e0798d522c62133c912a776dd'
